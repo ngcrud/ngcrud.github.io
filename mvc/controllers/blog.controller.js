a17ffd92e2ca0aa6b391d0f366da630627997649
Md Arifur Rahman <arif@newgen-bd.com>
2018-09-26 17:13:43 +0600

    testsaa

--- mvc/controllers/blog.controller.js
+++ mvc/controllers/blog.controller.js
@@ -5,7 +5,7 @@
 	BlogController.$inject = [ '$scope','$http' ];
 
 	function BlogController($scope,$http) {
-		$http.get('mvc/models/blogs.json').then(function(response) {
+		$http.get('mvc/models/blog.json').then(function(response) {
 			$scope.headerMessage = response.data.headerMessage;
 			$scope.welcomeMessage = response.data.welcomeMessage;
 			$scope.authors = response.data.authors;
